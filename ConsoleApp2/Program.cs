bc2e0fca00a17fcae9c1a9019310002184bc9fe4
RasulRustamli <rasular@code.edu.az>
2023-09-26 16:16:42 +0400

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+//if (select_number == '1')
+//{
+//    Console.WriteLine("Azerbaycan dili secildi");
+//}
+//else if (select_number == '2')
+//{
+//    Console.WriteLine("Rus dili secildi");
+//}
+//else if (select_number == '0')
+//{
+//    Console.WriteLine("Operatora baglanir");
+//}
+//else
+//{
+//    Console.WriteLine("Duzgun secim edin");
+//}
+int select_number = 1;
+
+switch (select_number)
+{
+    case 1:
+        Console.WriteLine("Azerbaycan dili secildi");
+        break;
+    case 2:
+        Console.WriteLine("Rus dili secildi");
+        break;
+    case 0:
+        Console.WriteLine("Operatora baglanir");
+        break;
+    default:
+        Console.WriteLine("Duzgun secim edin");
+        break;
+}
